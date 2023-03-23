9a966a193e643b3847c11fdf63e558170e512e84
Harshwardhan Praveen <praveenharsh01@gmail.com>
2023-03-23 15:35:34 -0400

    Moved files/Added greenlearning example
    Former-commit-id: 115e95d5f13519f59a69cac7585c20baf9099423

--- test/cheb2test.m
+++ test/cheb2test.m
@@ -2,33 +2,35 @@ clc
 clear
 close all
 
+eps = 1e-10;
+
 tic
-poisson = chebfun2(@(x,y) green(x,y),[0 1 0 1],'splitting','on');
+poisson = chebfun2(@(x,y) green(x,y),[0 1 0 1],'eps',eps);
 toc
 
 tic
 [U, S, V] = svd(poisson);
 toc
 
-% k = 100;
-% poissonk = U(:,1:k) * S(1:k,1:k) * V(:,1:k)';
-% 
-% figure;
-% plot(poisson)
-% colorbar;
-% xlabel('x','FontSize',15)
-% ylabel('s','FontSize',15)
-% % title('G(x,y) for Poisson problem')
-% 
-% figure;
-% plot(poissonk)
-% colorbar;
-% xlabel('x','FontSize',15)
-% ylabel('s','FontSize',15)
-% % title(['Rank ',num2str(k),' G(x,y) for Poisson problem'])
-% 
-% error = poisson-poissonk;
-% enorm = norm(error);
-% renorm = enorm/norm(poisson);
-% disp(enorm);
-% disp(renorm);
+k = 100;
+poissonk = U(:,1:k) * S(1:k,1:k) * V(:,1:k)';
+
+figure;
+plot(poisson)
+colorbar;
+xlabel('x','FontSize',15)
+ylabel('s','FontSize',15)
+% title('G(x,y) for Poisson problem')
+
+figure;
+plot(poissonk)
+colorbar;
+xlabel('x','FontSize',15)
+ylabel('s','FontSize',15)
+% title(['Rank ',num2str(k),' G(x,y) for Poisson problem'])
+
+error = poisson-poissonk;
+enorm = norm(error);
+renorm = enorm/norm(poisson);
+disp(enorm);
+disp(renorm);
