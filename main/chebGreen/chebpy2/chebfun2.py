Precision bug fixes, add plotting code
Former-commit-id: 662f146a92e6367e0330f578713176185657899f

main/chebGreen/chebpy2/chebfun2.py
@@ -42,10 +42,9 @@ class Chebfun2(ABC):
             check = (g[0].shape[1] == g[1].shape[0] and g[2].shape[0] == g[1].shape[0])
             if check:
                 assert check and not np.count_nonzero(g[1] == 0), \
-                    " ".join("Chebfun2 takes the input [C D R] such the bivariate function it \
-                             represents is \ f = C * diag(D) * R, where u is a column quasimatrix, \
-                             D has no zeros, and vt is a row quasimatrix.".split())
-
+                    " ".join("""Chebfun2 takes the input [C D R] such the bivariate function it
+                             represents is f = C * diag(D) * R, where u is a column quasimatrix,
+                             D has no zeros, and vt is a row quasimatrix.""".split())
             self.cols, self.pivotValues, self.rows = g[0], 1/g[1], g[2]
             self.domain = np.hstack([self.rows.domain,self.cols.domain])
         else:
@@ -413,7 +412,7 @@ class Chebfun2(ABC):
         """
         Computes the integral transform on input function g (chebfun), considering the chebfun2, f,
         as a kernel.
-        \int f(x,y) g(x) dx
+        \\int f(x,y) g(x) dx
         """
         f = self
         cols = f.cols

main/chebGreen/chebpy2/preferences.py
@@ -12,5 +12,5 @@ class Chebpy2Preferences(ABC):
         
         self.prefx = chebpy.core.settings.ChebPreferences()
         self.prefy = chebpy.core.settings.ChebPreferences()
-        self.prefx.eps = 1e-8
-        self.prefy.eps = 1e-8
+        self.prefx.eps = np.finfo(np.float32).eps
+        self.prefy.eps = np.finfo(np.float32).eps

main/chebGreen/greenlearning/model.py
@@ -3,7 +3,7 @@ from .activations import get_activation
 from .loss import LossGreensFunction
 from .utils import approximateDistanceFunction
 
-
+MODEL_SAVE_FREQUENCY = 50
 
 class NN(torch.nn.Module):
     def __init__(self,
@@ -128,7 +128,8 @@ class GreenNN(ABC):
               data,
               epochs = {'adam':parser['GREENLEARNING'].getint('epochs_adam') ,
                         'lbfgs':parser['GREENLEARNING'].getint('epochs_lbfgs')},
-              trainHomogeneous = True
+              trainHomogeneous = True,
+              savePath = "temp"
               ):
         """
         Method to train the neural network.
@@ -161,7 +162,8 @@ class GreenNN(ABC):
         self.init_loss(data.xF, data.xU) # Initialize the loss function
 
         lossHistory = {'training': [], 'validation':[]} # Initialize a dictionary to store the loss history
-
+        
+        bestLoss = np.inf
         print("Training with Adam:")
         for epoch in range(int(epochs['adam'])):
             fTrain, uTrain = data.trainDataset
@@ -178,6 +180,11 @@ class GreenNN(ABC):
 
             # Change this to be an average over batches. Currently assuming a single batch
             lossHistory['training'].append(lossValue.item())
+
+            # Save the model if the validation loss is the better than the previous best loss
+            if (epoch+1)%MODEL_SAVE_FREQUENCY == 0 and lossHistory['validation'][-1] < bestLoss:
+                bestLoss = lossHistory['validation'][-1]
+                self.saveModels(path = savePath)
 
             with torch.no_grad():
                 fVal, uVal = data.valDataset
@@ -208,12 +215,22 @@ class GreenNN(ABC):
             # Change this to be an average over batches. Currently assuming a single batch
             lossHistory['training'].append(lossValue.item())
 
+            # Save the model if the validation loss is the better than the previous best loss
+            if (epoch+1)%MODEL_SAVE_FREQUENCY == 0 and lossHistory['validation'][-1] < bestLoss:
+                bestLoss = lossHistory['validation'][-1]
+                self.saveModels(path = savePath)
+
             with torch.no_grad():
                 fVal, uVal = data.valDataset
                 lossValue = self.lossfn(fVal.to(device), uVal.to(device), trainHomogeneous)
             lossHistory['validation'].append(lossValue.item())
             if (epoch+1) % 10 == 0:
                 print(f"Loss at epoch {epoch+1}: Training = {lossHistory['training'][-1]:.3E}, Validation = {lossHistory['validation'][-1]:.3E}")
+
+        # Fallback to save the last model if the MODEL_SAVE_FREQUENCY is not reached
+            if lossHistory['validation'][-1] < bestLoss:
+                bestLoss = lossHistory['validation'][-1]
+                self.saveModels(path = savePath)
 
         return lossHistory
     

main/chebGreen/utils.py
@@ -1,5 +1,5 @@
 from .backend import os, sys, Path, MATLABPath, parser, np, tempfile, config
-from chebGreen.chebpy2.chebpy.core.algorithms import chebpts2
+from chebGreen.chebpy2.chebpy.core.algorithms import chebpts2, vals2coeffs2, standard_chop
 from chebGreen.chebpy2.chebpy.api import chebfun
 from chebGreen.chebpy2.chebpy.core.settings import ChebPreferences
 
@@ -95,7 +95,21 @@ def vec2cheb(f, x, domain = None):
 
     prefs = ChebPreferences()
     prefs.eps = np.finfo(config(np)).eps
-    return chebfun(fc, domain, prefs = prefs)
+
+    coeffs_fc = vals2coeffs2(fc)
+
+    # Find tolerance for standard chop
+    a, b = domain
+    h = max(np.linalg.norm(domain, np.inf),1)
+    hF = b - a
+    hscale = max(h / hF, 1)
+    tol = prefs.eps * hscale
+
+    # Determine where to chop the coefficients
+    npts = standard_chop(coeffs_fc, tol)
+
+    cbfun = chebfun(coeffs_fc[:npts].reshape(-1,1), domain, prefs = prefs, initcoeffs = True)
+    return cbfun
 
 def computeEmpiricalError(data, G, N = None):
     RE, UC, U0 = [],[],[]