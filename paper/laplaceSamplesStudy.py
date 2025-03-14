456200845cfc084afe8984f2286f8f53b0cdf56f
Harshwardhan Praveen <praveenharsh01@gmail.com>
2025-03-14 19:59:05 -0400

    Add study for number of samples

---
+++ paper/laplaceSamplesStudy.py
@@ -0,0 +1,93 @@
+from chebgreen.greenlearning.utils import DataProcessor
+from chebgreen.greenlearning.model import *
+from chebgreen.backend import plt
+from chebgreen.utils import runCustomScript
+
+from chebgreen.chebpy2 import Chebfun2, Chebpy2Preferences
+from chebgreen.chebpy2.chebpy.core.settings import ChebPreferences
+from chebgreen.chebpy2.chebpy import chebfun, Chebfun
+import shutil, time, sys
+
+def green(x,s):
+    g = 0
+    g = (x <= s) * (x * (1-s)) + (x > s) * (s * (1-x))
+    return g
+
+N_samples = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+noise_level = 0.0
+
+example = 'laplace'
+script = 'generate_example'
+theta = None
+Nsample = 100
+lmbda = 0.01
+Nf = 500
+Nu = 500
+
+dimension = 1
+domain = [0,1,0,1]
+layerConfig = [50,50,50,50]
+activation = 'rational'
+dirichletBC = True
+
+t0 = time.perf_counter()
+
+print(f"Learning a chebfun model for analytical Green's function of Laplacian operator")
+# Analytical Green's function
+eps = 1e-6
+cheb2prefs = Chebpy2Preferences()
+cheb2prefs.prefx.eps = eps
+cheb2prefs.prefx.eps = eps
+g = Chebfun2(green, domain = domain, prefs = cheb2prefs, simplify = True)
+gnorm = g.norm()
+print("-------------------------------------------------------------------------------")
+
+Error = []
+for N_samples in N_samples:
+    runCustomScript(script,example,theta,Nsample,lmbda,Nf,Nu,noise_level)
+    data = DataProcessor(f"datasets/{example}/data.mat")
+    data.generateDataset(trainRatio = 0.95)
+
+    model = GreenNN()
+
+    model.build(dimension, domain, layerConfig, activation, dirichletBC)
+
+    print("-------------------------------------------------------------------------------")
+    print(f"Training greenlearning model for example \'{example}\' with {N_samples} samples")
+    lossHistory = model.train(data, epochs = {'adam':int(2000), 'lbfgs':int(0)})
+
+    xF, xU = data.xF, data.xU
+    x, y = np.meshgrid(xU, xF)
+    G = model.evaluateG(x,y)
+    N = model.evaluateN(data.xF)
+    GreenNNPath = "temp/"
+    model.build(dimension = dimension,domain = domain, dirichletBC = dirichletBC, loadPath = GreenNNPath, device = torch.device('cpu'))
+
+    savePath = f"plots/{example}"
+    Path(savePath).mkdir(parents=True, exist_ok=True)
+    
+    fig = plt.figure(figsize = (8,6))
+    fig.savefig(f'{savePath}/{example}-{int(N_samples)}.png', dpi = fig.dpi)
+
+    print("-------------------------------------------------------------------------------")
+    print(f"Learning a chebfun model for example \'{example}\' with {N_samples} samples")
+    # ChebGreen
+    cheb2prefs = Chebpy2Preferences()
+    Gcheb = Chebfun2(model.evaluateG, domain = model.domain, prefs = cheb2prefs, simplify = True)
+
+    print("-------------------------------------------------------------------------------")
+    print(f"Computing empirical error for example \'{example}\' with {N_samples} samples")
+    
+    # Compute the empirical error
+    cheb2prefs = Chebpy2Preferences()
+    cheb2prefs.prefx.eps = eps
+    cheb2prefs.prefx.eps = eps
+    e = Chebfun2(lambda x,y: Gcheb[x,y] - green(x,y), domain = model.domain, prefs = cheb2prefs, simplify = True)
+    
+    Error.append(e.norm()/gnorm)
+    print(f"Error for a model with {N_samples} samples is {Error[-1]*100}%")
+    print("-------------------------------------------------------------------------------")
+    shutil.rmtree(f"datasets/{example}")
+for error in Error:
+    print(f"{error*100:.2f}")
+t1 = time.perf_counter()
